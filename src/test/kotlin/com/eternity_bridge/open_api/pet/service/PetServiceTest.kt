abc25a0e8f56a4aff49dcaf484bfbe7b23fabe35
jungdonguk <foodsmith96@naver.com>
2025-06-18 23:19:36 +0900

    TEST - 반려동물 조회 기능 테스트 코드 추가

--- src/test/kotlin/com/eternity_bridge/open_api/pet/service/PetServiceTest.kt
+++ src/test/kotlin/com/eternity_bridge/open_api/pet/service/PetServiceTest.kt
@@ -1,21 +1,14 @@
 package com.eternity_bridge.open_api.pet.service
 
-import com.eternity_bridge.open_api.pet.dto.CreatePetRequest
 import com.eternity_bridge.open_api.pet.enums.PetType
 import com.eternity_bridge.open_api.pet.repository.PetRepository
-import com.navercorp.fixturemonkey.FixtureMonkey
-import com.navercorp.fixturemonkey.kotlin.KotlinPlugin
-import com.navercorp.fixturemonkey.kotlin.giveMeBuilder
-import com.navercorp.fixturemonkey.kotlin.giveMeOne
 import io.kotest.core.spec.style.FunSpec
 import io.kotest.matchers.shouldBe
 import org.slf4j.MDC
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.test.context.ActiveProfiles
-import org.springframework.transaction.annotation.Transactional
 
-@Transactional
 @ActiveProfiles("test")
 @SpringBootTest
 class PetServiceTest(
@@ -29,8 +22,10 @@ class PetServiceTest(
         }
 
         afterTest {
+            petRepository.deleteAll()
             MDC.clear()
         }
+
 
         test("반려동물이 성공적으로 생성된다.") {
             // given
@@ -47,6 +42,26 @@ class PetServiceTest(
             petRepository.existsById(result) shouldBe true
             petRepository.count() shouldBe 1L
             result shouldBe 1L
+        }
+
+
+        test("반려동물이 성공적으로 조회된다.") {
+            // given
+            val request = PetFixtureFactory.createPetRequest(
+                name = "보나",
+                petType = PetType.DOG,
+            )
+            val memberId = 1L
+            val petId = petService.createPet(request, memberId)
+
+            // when
+            val result = petService.getPet(petId)
+
+            // then
+            result.id shouldBe 2L
+            result.id shouldBe petId
+            result.name shouldBe request.name
+            result.petType shouldBe request.petType
         }
     }
 
